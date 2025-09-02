d611f51c145a0f5731659c26b583b5e4febe92cc
Jaiver Martinez Bonilla <jamarbo@hotmail.com>
2025-09-02 00:08:31 -0500

    chore(types): update vite env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_API_BASE?: string
+  readonly VITE_FORCE_ADMIN?: string
+  readonly VITE_ADMIN_EMAILS?: string
 }
 
 interface ImportMeta {
